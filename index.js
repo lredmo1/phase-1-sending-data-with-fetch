8a703d94831f9e2a90c9e65e63d5d47614579c06
Lisa <redlisa125@gmail.com>
2021-10-03 12:02:01 -1000

    commit tree dda9073d5356292237f6a3cd812a0445cf9f19f9

--- index.js
+++ index.js
@@ -1,1 +1,76 @@
 // Add your code here
+//why do the function calls inside the functions need the parameters? 
+
+function formDataObject(userName, userEmail) {
+    return {
+        name: userName,
+        email: userEmail
+    }
+}
+
+
+
+function submitData(userName, userEmail) {
+    return fetch("http://localhost:3000/users", {
+        method: 'POST',
+        headers: {
+            'Content-Type': "application/json",
+            Accept: "application/json"
+        },
+        body: JSON.stringify(formDataObject(userName, userEmail))
+    })
+        .then(resp => resp.json())
+        .then(objectResponse => {
+            let newID = objectResponse.id
+            const newIDNumber = document.createElement('p')
+            newIDNumber.textContent = newID
+            let bodyTag = document.querySelector('body')
+            bodyTag.appendChild(newIDNumber)
+
+        })
+        .catch(message => {
+            let bodyTag = document.querySelector('body')
+            bodyTag.append(message)
+
+        })
+}
+
+submitData("test", "test@email.com")
+
+// function formDataObject(userName, userEmail) {
+//     return {
+//         name: userName,
+//         email: userEmail
+//     }
+// }
+
+// function functionObjectThing(userName, userEmail) {
+//     return {
+//         method: 'POST',
+//         headers: {
+//             'Content-Type': "application/json",
+//             Accept: "application/json"
+//         },
+//         body: JSON.stringify(formDataObject(userName, userEmail))
+//     }
+// }
+
+// function submitData(userName, userEmail) {
+//     return fetch("http://localhost:3000/users", functionObjectThing(userName, userEmail))
+//         .then(resp => resp.json())
+//         .then(objectResponse => {
+//             let newID = objectResponse.id
+//             const newIDNumber = document.createElement('p')
+//             newIDNumber.textContent = newID
+//             let bodyTag = document.querySelector('body')
+//             bodyTag.appendChild(newIDNumber)
+
+//         })
+//         .catch(message => {
+//             let bodyTag = document.querySelector('body')
+//             bodyTag.append(message)
+
+//         })
+// }
+
+// submitData()
